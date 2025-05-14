851d4ad48d0e273fb18b816e90b2cc6171d4665d
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-14 17:32:18 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: May 2025
+
 // This file contains the JS functions for index.html
+
+"use-strict"
+
+/**
+* This function calculate, doing multiplication
+*/
+// eslint-disable-next-line no-unused-vars
+function cookieClicker () {
+  //input 
+  let numberOfCoockies = 0
+  let coockieCount = localStorage.getItem('cookieClick')
+
+  //process
+  if (cookieClick) {
+    coockieCount = Number(coockieCount) + 1
+  } else {
+    coockieCount = 1
+  }
+
+  // output
+
+  document.getElementById('answer').innerHTML = 'Coockies: ' + coockieCount
+}
