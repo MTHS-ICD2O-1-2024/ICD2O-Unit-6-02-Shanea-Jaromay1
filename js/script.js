b45405c9f39d562f84eac605ff5389d5b6a68861
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-20 17:13:50 +0000

    finally

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 Ain Jeong All rights reserved
+// Copyright (c) 2025 Shanea Jaromay All rights reserved
 //
 // Created by: Shanea Jaromay
 // Created on: May 2025
@@ -12,18 +12,18 @@
 */
 // eslint-disable-next-line no-unused-vars
 function cookieClicker () {
-  //input 
-  let numberOfCoockies = 0
-  let coockieCount = localStorage.getItem('cookieClick')
+ //input
+  let cookieCount = localStorage.getItem('cookieClickCount')
 
-  //process
-  if (cookieClick) {
-    coockieCount = Number(coockieCount) + 1
+ //process
+  if (cookieCount != null) {
+    cookieCount = Number(cookieCount) + 1
   } else {
-    coockieCount = 1
+    cookieCount = 1
   }
 
-  // output
+localStorage.setItem('cookieClickCount', cookieClickCount)
 
-  document.getElementById('answer').innerHTML = 'Coockies: ' + coockieCount
+ // output
+  document.getElementById('answer').innerHTML = 'Cookies: ' + cookieCount
 }
